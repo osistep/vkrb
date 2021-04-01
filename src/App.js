4ce8e4e13287575c6cf7dbd68be73f8a1128f8ab
OsipenkoSA <steposimus@gmail.com>
2021-04-01 04:34:05 +0300

    Update index page

--- src/App.js
+++ src/App.js
@@ -15,18 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current from server time is {currentTime}.</p>
+        <p>The current server time is {currentTime}.</p>
       </header>
     </div>
   );
