54101ebd29dc272c558e3cdbae0515b2c4059361
OsipenkoSA <steposimus@gmail.com>
2021-04-01 04:21:04 +0300

    Another prod test

--- backend/api.py
+++ backend/api.py
@@ -3,6 +3,7 @@ from flask import Flask
 
 app = Flask(__name__, static_folder="../build", static_url_path='/')
 
+# Main route
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
